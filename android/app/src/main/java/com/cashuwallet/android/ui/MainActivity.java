a07caa215a91877a05aea6590ba21c6bce493808
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2018-07-30 19:24:33 -0300

    I have commented the code to protect the app from screenshots, should reenable when out of beta phase

--- android/app/src/main/java/com/cashuwallet/android/ui/MainActivity.java
+++ android/app/src/main/java/com/cashuwallet/android/ui/MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
 
         setContentView(R.layout.activity_main);
 
